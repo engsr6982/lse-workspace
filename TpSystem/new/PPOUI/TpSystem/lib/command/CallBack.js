71a1c88965fde0590fbace5acfcb9d373b05010b
engsr6982 <pwxg286704@163.com>
2023-05-21 10:38:04 +0800

    完成命令注册

--- TpSystem/new/PPOUI/TpSystem/lib/command/CallBack.js
+++ TpSystem/new/PPOUI/TpSystem/lib/command/CallBack.js
@@ -1,11 +1,55 @@
-import { Main } from "../form/Mian.js";
-import { MainUI } from "../cache.js";
+import { MAPPING_TABLE, Main } from "../form/Mian.js";
+import { FileOperation, MainUI, PlayerSeting } from "../cache.js";
 import { SetingForm } from "../form/Seting_Mgr.js";
+import { RandomTeleportCore } from "../core/TPR.js";
+import { HomeForm } from "../form/Home.js";
+import { WarpForm } from "../form/Warp.js";
+import { TPRForm } from "../form/TPR.js";
 
 export function CallBack(_, ori, out, res) {
     switch (res.action) {
         case "mgr":
+            if (ori.type !== 0) return out.error('此命令仅限玩家执行');
+            if (!ori.player.isOP()) return out.error('仅限管理员使用');
             SetingForm(ori.player);
+            break;
+        case "reload":
+            if (ori.type !== 7) return out.error('此命令仅限控制台执行');
+            FileOperation.saveFile();
+            out.success('---操作完成---');
+            break;
+        case "tpr":
+            if (!ori.player) return out.error('获取玩家对象失败！');
+            RandomTeleportCore(ori.player);
+            break;
+        case 'refresh':
+            if (ori.type !== 7) return out.error('此命令仅限控制台执行');
+            FileOperation.readFile();
+            out.success('---操作完成---');
+            break;
+        case 'back':
+            if (!ori.player) return out.error('获取玩家对象失败！');
+            MAPPING_TABLE['DeathUi'](ori.player);
+            break;
+        case 'gui':
+            if (!ori.player) return out.error('获取玩家对象失败！');
+            const Table = {
+                home: HomeForm.Panel,
+                warp: WarpForm,
+                // player: Forms.PlayerTransportation,
+                death: TPRForm,
+                tpr: TPRForm,
+                seting: PlayerSeting
+            }
+            if (res.gui_name) {
+                Table[res.gui_name](ori.player);
+            } else {
+                Main(ori.player, MainUI);
+            }
+            break;
+        case 'accept':
+            break;
+        case "deny":
             break;
         default: Main(ori.player, MainUI);
     }
--- TpSystem/new/PPOUI/TpSystem/lib/command/RegCommand.js
+++ TpSystem/new/PPOUI/TpSystem/lib/command/RegCommand.js
@@ -7,14 +7,36 @@ export function RegCommand() {
     cmd.setEnum('mgr', ['mgr']);
     cmd.mandatory('action', ParamType.Enum, 'mgr', 1);
     cmd.overload(['mgr']);
-
     // tps [gui] [home|warp|player|death|random|seting]
     cmd.setEnum("gui", ['gui']);
     cmd.optional('action', ParamType.Enum, 'gui', 1);
-    cmd.setEnum('gui_enum', ['home', 'warp', 'player', 'death', 'random', 'seting']);
+    cmd.setEnum('gui_enum', ['home', 'warp',/*  'player',  */'death', 'tpr', 'seting']);
     cmd.optional('gui_name', ParamType.Enum, 'gui_enum', 'gui_enum', 1);
     cmd.overload(['gui', 'gui_enum']);
-
+    //tps reload
+    cmd.setEnum('reload', ['reload']);
+    cmd.mandatory('action', ParamType.Enum, 'reload', 1);
+    cmd.overload(['reload']);
+    //tps accept
+    cmd.setEnum('accept', ['accept']);
+    cmd.mandatory('action', ParamType.Enum, 'accept', 1);
+    cmd.overload(['accept']);
+    //tps deny
+    cmd.setEnum('deny', ['deny']);
+    cmd.mandatory('action', ParamType.Enum, 'deny', 1);
+    cmd.overload(['deny']);
+    //tps back
+    cmd.setEnum('back', ['back']);
+    cmd.mandatory('action', ParamType.Enum, 'back', 1);
+    cmd.overload(['back']);
+    //tps refresh
+    cmd.setEnum('refresh', ['refresh']);
+    cmd.mandatory('action', ParamType.Enum, 'refresh', 1);
+    cmd.overload(['refresh']);
+    //tps tpr
+    cmd.setEnum('tpr', ['tpr']);
+    cmd.mandatory('action', ParamType.Enum, 'tpr', 1);
+    cmd.overload(['tpr']);
     cmd.setCallback(CallBack);
     cmd.setup();
 }
